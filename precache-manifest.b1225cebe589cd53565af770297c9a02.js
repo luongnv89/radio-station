5b7943e508659948e4e036195039847be281acdd
Luong NGUYEN <luongnv89@gmail.com>
2018-11-04 02:18:58 +0100

    Update new version

---
+++ precache-manifest.b1225cebe589cd53565af770297c9a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecb7099790c45795065d",
+    "url": "./static/css/main.5c2a79fd.chunk.css"
+  },
+  {
+    "revision": "ecb7099790c45795065d",
+    "url": "./static/js/main.ecb70997.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "./static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7e334a8528c1f9c3b09b9ac03c5d9a4e",
+    "url": "./index.html"
+  }
+];
